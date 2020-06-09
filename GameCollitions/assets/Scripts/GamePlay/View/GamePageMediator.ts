af6f4be1296feb41fdef5cef75c37e6d831ee9c3
eatdreamcat <meow.std@outlook.com>
2020-06-09 16:35:31 +0800

    version 3

--- GameCollitions/assets/Scripts/GamePlay/View/GamePageMediator.ts
+++ GameCollitions/assets/Scripts/GamePlay/View/GamePageMediator.ts
@@ -39,7 +39,7 @@ export class GamePageMediator extends BaseMediator<GamePageView> {
         let gameUrl = special ? GameConfig.inst.Config.specialPath : GameConfig.inst.Config.normalPath;
         this.WebView.node.active = true;
 
-        this.WebView.url = GameConfig.inst.Config.Url + gameUrl + name;
+        this.WebView.url = GameConfig.inst.Config.Url + gameUrl + name + "?time=" + Date.now();
         console.log(this.WebView.url);
         this.View.Show();
 
