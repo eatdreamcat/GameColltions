5e5c97ee629ba8d800992be7337a59308f6c7745
eatdreamcat <meow.std@outlook.com>
2020-06-30 18:37:21 +0800

    aaa

--- GameCollitions/manifest.bat
+++ GameCollitions/manifest.bat
@@ -1,2 +1,2 @@
-node version_generator.js -v 1.0.5 -u https://vicat.wang/Remote-Hot-Update/Arcade/ -s ./  -d ./assets/remote-assets/
+node version_generator.js -v 1.0.6 -u https://vicat.wang/Remote-Hot-Update/Arcade/ -s ./  -d ./assets/remote-assets/
 pause
