9e9d13a5814b213303fcaf3005added5a41b1c77
eatdreamcat <meow.std@outlook.com>
2021-03-18 16:27:40 +0800

    aasddsa

--- GameCollitions/assets/Scripts/GamePlay/View/GameListView.ts
+++ GameCollitions/assets/Scripts/GamePlay/View/GameListView.ts
@@ -8,265 +8,281 @@
 //  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
-
 import { GameListMediator } from "./GameListMediator";
 import BaseView from "../../View/BaseView";
 import { GameConfig } from "../../Global/GameConfig";
-import { LoadGameSignal, LoadNativeGameSignal } from "../Command/LoadGameSignal";
+import {
+  LoadGameSignal,
+  LoadNativeGameSignal,
+} from "../Command/LoadGameSignal";
 import { networkInterfaces } from "os";
 
 const { ccclass, property } = cc._decorator;
 
-
 enum Type {
-    Normal,
-    Special,
-    Native
+  Normal,
+  Special,
+  Native,
 }
 @ccclass
 export default class GameListView extends BaseView {
+  private type: Type = Type.Special;
 
+  private readonly SELECTED_COLOR = cc.color(12, 130, 254);
+  onLoad() {
+    super.onLoad();
 
+    this.type = Type.Normal;
 
+    // 默认选中特殊
 
-    private type: Type = Type.Special;
+    this.updateType();
 
-    private readonly SELECTED_COLOR = cc.color(12, 130, 254)
-    onLoad() {
-        super.onLoad();
+    this.SpecialNode.on(
+      cc.Node.EventType.TOUCH_START,
+      () => {
+        this.Special.color = this.SELECTED_COLOR;
+      },
+      this
+    );
 
-        this.type = Type.Special;
-
-        // 默认选中特殊
-
+    this.SpecialNode.on(
+      cc.Node.EventType.TOUCH_CANCEL,
+      () => {
         this.updateType();
+      },
+      this
+    );
 
+    this.SpecialNode.on(
+      cc.Node.EventType.TOUCH_END,
+      () => {
+        this.type = Type.Special;
+        this.updateType();
+        this.reFreshItems();
+      },
+      this
+    );
 
-        this.SpecialNode.on(cc.Node.EventType.TOUCH_START, () => {
+    this.NormalNode.on(
+      cc.Node.EventType.TOUCH_START,
+      () => {
+        this.Normal.color = this.SELECTED_COLOR;
+      },
+      this
+    );
 
-            this.Special.color = this.SELECTED_COLOR;
+    this.NormalNode.on(
+      cc.Node.EventType.TOUCH_END,
+      () => {
+        this.type = Type.Normal;
+        this.updateType();
+        this.reFreshItems();
+      },
+      this
+    );
 
-        }, this);
+    this.NormalNode.on(
+      cc.Node.EventType.TOUCH_CANCEL,
+      () => {
+        this.updateType();
+      },
+      this
+    );
 
-        this.SpecialNode.on(cc.Node.EventType.TOUCH_CANCEL, () => {
+    this.NativeNode.on(
+      cc.Node.EventType.TOUCH_START,
+      () => {
+        this.Native.color = this.SELECTED_COLOR;
+      },
+      this
+    );
 
-            this.updateType();
+    this.NativeNode.on(
+      cc.Node.EventType.TOUCH_END,
+      () => {
+        this.type = Type.Native;
+        this.updateType();
+        this.reFreshItems();
+      },
+      this
+    );
 
-        }, this);
+    this.NativeNode.on(
+      cc.Node.EventType.TOUCH_CANCEL,
+      () => {
+        this.updateType();
+      },
+      this
+    );
 
-        this.SpecialNode.on(cc.Node.EventType.TOUCH_END, () => {
-            this.type = Type.Special;
-            this.updateType();
-            this.reFreshItems();
-        }, this);
+    this.Icon.active = false;
+    this.Icon.getChildByName("new").scale = 0;
 
+    this.BindMedaitor(GameListMediator);
+  }
 
-        this.NormalNode.on(cc.Node.EventType.TOUCH_START, () => {
+  updateType() {
+    switch (this.type) {
+      case Type.Special:
+        this.Special.color = this.SELECTED_COLOR;
+        this.Normal.color = cc.Color.WHITE;
+        this.Native.color = cc.Color.WHITE;
+        break;
+      case Type.Normal:
+        this.Special.color = cc.Color.WHITE;
+        this.Native.color = cc.Color.WHITE;
+        this.Normal.color = this.SELECTED_COLOR;
+        break;
+      case Type.Native:
+        this.Special.color = cc.Color.WHITE;
+        this.Native.color = this.SELECTED_COLOR;
+        this.Normal.color = cc.Color.WHITE;
+        break;
 
-            this.Normal.color = this.SELECTED_COLOR;
-        }, this);
+      default:
+        break;
+    }
+  }
 
-        this.NormalNode.on(cc.Node.EventType.TOUCH_END, () => {
+  get ScrollView() {
+    return this.getComponent(cc.ScrollView);
+  }
 
-            this.type = Type.Normal;
-            this.updateType();
-            this.reFreshItems();
-        }, this);
+  get Content() {
+    return this.ScrollView.content;
+  }
 
-        this.NormalNode.on(cc.Node.EventType.TOUCH_CANCEL, () => {
+  get Icon() {
+    return this.node.getChildByName("Icon");
+  }
 
-            this.updateType();
+  getIconSprite(node: cc.Node) {
+    return node
+      .getChildByName("Mask")
+      .getChildByName("icon512")
+      .getComponent(cc.Sprite);
+  }
 
-        }, this);
+  get Selection() {
+    return this.node.getChildByName("TypeSelection");
+  }
 
-        this.NativeNode.on(cc.Node.EventType.TOUCH_START, () => {
+  get Normal() {
+    return this.Selection.getChildByName("Normal").getChildByName("Label");
+  }
 
-            this.Native.color = this.SELECTED_COLOR;
-        }, this);
+  get Special() {
+    return this.Selection.getChildByName("Special").getChildByName("Label");
+  }
 
-        this.NativeNode.on(cc.Node.EventType.TOUCH_END, () => {
+  get NormalNode() {
+    return this.Selection.getChildByName("Normal");
+  }
 
-            this.type = Type.Native;
-            this.updateType();
-            this.reFreshItems();
-        }, this);
+  get SpecialNode() {
+    return this.Selection.getChildByName("Special");
+  }
 
-        this.NativeNode.on(cc.Node.EventType.TOUCH_CANCEL, () => {
+  get isSpecial() {
+    return this.type == Type.Special;
+  }
 
-            this.updateType();
+  get Native() {
+    return this.Selection.getChildByName("Native").getChildByName("Label");
+  }
 
-        }, this);
+  get NativeNode() {
+    return this.Selection.getChildByName("Native");
+  }
 
+  iconCache: {} = {};
+  reFreshItems() {
+    let games = [];
 
-
-
-
-
-
-
-        this.Icon.active = false;
-        this.Icon.getChildByName("new").scale = 0;
-
-        this.BindMedaitor(GameListMediator);
-
-
+    switch (this.type) {
+      case Type.Native:
+        games = GameConfig.inst.Config.nativeGames;
+        break;
+      case Type.Normal:
+        games = GameConfig.inst.Config.normalGames;
+        break;
+      case Type.Special:
+        games = GameConfig.inst.Config.specialGames;
+        break;
     }
 
-    updateType() {
+    let newGames = GameConfig.inst.Config.new;
 
-        switch (this.type) {
-            case Type.Special:
-                this.Special.color = this.SELECTED_COLOR;
-                this.Normal.color = cc.Color.WHITE;
-                this.Native.color = cc.Color.WHITE;
-                break;
-            case Type.Normal:
-                this.Special.color = cc.Color.WHITE;
-                this.Native.color = cc.Color.WHITE;
-                this.Normal.color = this.SELECTED_COLOR;
-                break;
-            case Type.Native:
-                this.Special.color = cc.Color.WHITE;
-                this.Native.color = this.SELECTED_COLOR;
-                this.Normal.color = cc.Color.WHITE;
-                break;
+    let itemCount = games.length;
 
-            default:
-                break;
+    this.Content.removeAllChildren();
+
+    console.log(" reFreshItems: ", Type[this.type]);
+    let self = this;
+
+    function refreshIcon(node: cc.Node, index: number) {
+      index = index % games.length;
+
+      if (self.iconCache[games[index]]) {
+        initIcon(node, self.iconCache[games[index]], index);
+        return;
+      }
+
+      let loadFunc =
+        GameConfig.Url == ""
+          ? cc.loader.loadRes.bind(cc.loader)
+          : cc.loader.load.bind(cc.loader);
+      loadFunc(
+        GameConfig.Url + "Icons/" + games[index] + ".jpg?time=" + Date.now(),
+        (err, sp) => {
+          if (err) {
+            console.error(err);
+          } else {
+            self.iconCache[games[index]] = sp;
+            initIcon(node, sp, index);
+          }
         }
-
+      );
     }
 
-    get ScrollView() {
-        return this.getComponent(cc.ScrollView);
-    }
+    function initIcon(node: cc.Node, sp: cc.Texture2D, index: number) {
+      self.getIconSprite(node).spriteFrame = new cc.SpriteFrame(sp);
+      node.name = games[index];
 
-    get Content() {
-        return this.ScrollView.content;
-    }
+      node.targetOff(self);
 
-
-    get Icon() {
-        return this.node.getChildByName("Icon")
-    }
-
-    getIconSprite(node: cc.Node) {
-        return node.getChildByName("Mask").getChildByName("icon512").getComponent(cc.Sprite)
-    }
-
-
-    get Selection() {
-        return this.node.getChildByName("TypeSelection")
-    }
-
-    get Normal() {
-        return this.Selection.getChildByName("Normal").getChildByName("Label");
-    }
-
-    get Special() {
-        return this.Selection.getChildByName("Special").getChildByName("Label")
-    }
-
-    get NormalNode() {
-        return this.Selection.getChildByName("Normal")
-    }
-
-    get SpecialNode() {
-        return this.Selection.getChildByName("Special")
-    }
-
-    get isSpecial() {
-        return this.type == Type.Special;
-    }
-
-    get Native() {
-        return this.Selection.getChildByName("Native").getChildByName("Label")
-    }
-
-    get NativeNode() {
-        return this.Selection.getChildByName("Native")
-    }
-
-
-    iconCache: {} = {}
-    reFreshItems() {
-        let games = [];
-
-        switch (this.type) {
-            case Type.Native:
-                games = GameConfig.inst.Config.nativeGames;
-                break;
-            case Type.Normal:
-                games = GameConfig.inst.Config.normalGames;
-                break;
-            case Type.Special:
-                games = GameConfig.inst.Config.specialGames;
-                break;
-        }
-
-        let newGames = GameConfig.inst.Config.new;
-
-        let itemCount = games.length;
-
-        this.Content.removeAllChildren();
-
-
-
-        console.log(" reFreshItems: ", Type[this.type]);
-        let self = this;
-
-        function refreshIcon(node: cc.Node, index: number) {
-            index = index % games.length;
-
-            if (self.iconCache[games[index]]) {
-                initIcon(node, self.iconCache[games[index]], index);
-                return;
+      node.runAction(
+        cc.sequence(
+          cc.scaleTo(0.1, 1),
+          cc.callFunc(() => {
+            if (newGames.indexOf(node.name) >= 0) {
+              node.getChildByName("new").runAction(cc.scaleTo(0.1, 1));
             }
 
-            let loadFunc = GameConfig.Url == "" ? cc.loader.loadRes.bind(cc.loader) : cc.loader.load.bind(cc.loader);
-            loadFunc(GameConfig.Url + "Icons/" + games[index] + ".jpg?time=" + Date.now(), (err, sp) => {
-                if (err) {
-                    console.error(err);
+            node.on(
+              cc.Node.EventType.TOUCH_END,
+              () => {
+                if (self.type == Type.Native) {
+                  LoadNativeGameSignal.inst.dispatchOne(node.name);
                 } else {
-                    self.iconCache[games[index]] = sp;
-                    initIcon(node, sp, index);
+                  LoadGameSignal.inst.dispatchTwo(node.name, self.isSpecial);
                 }
-            })
-        }
-
-        function initIcon(node: cc.Node, sp: cc.Texture2D, index: number) {
-
-            self.getIconSprite(node).spriteFrame = new cc.SpriteFrame(sp);
-            node.name = games[index];
-
-            node.targetOff(self);
-
-
-            node.runAction(cc.sequence(cc.scaleTo(0.1, 1), cc.callFunc(() => {
-                if (newGames.indexOf(node.name) >= 0) {
-                    node.getChildByName("new").runAction(cc.scaleTo(0.1, 1))
-                }
-
-                node.on(cc.Node.EventType.TOUCH_END, () => {
-                    if (self.type == Type.Native) {
-                        LoadNativeGameSignal.inst.dispatchOne(node.name);
-                    } else {
-                        LoadGameSignal.inst.dispatchTwo(node.name, self.isSpecial);
-                    }
-
-                }, self);
-            })))
-        }
-
-        while (this.Content.childrenCount < itemCount) {
-            let item = cc.instantiate(this.Icon);
-            item.getChildByName("new").scale = 0;
-            item.active = true;
-            item.scale = 0;
-            this.Content.addChild(item);
-            refreshIcon(item, this.Content.childrenCount - 1);
-        }
+              },
+              self
+            );
+          })
+        )
+      );
     }
 
-
+    while (this.Content.childrenCount < itemCount) {
+      let item = cc.instantiate(this.Icon);
+      item.getChildByName("new").scale = 0;
+      item.active = true;
+      item.scale = 0;
+      this.Content.addChild(item);
+      refreshIcon(item, this.Content.childrenCount - 1);
+    }
+  }
 }
